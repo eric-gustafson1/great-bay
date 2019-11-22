a82c131c7388dd3739509e5d1061ec46620c9336
Eric Gustafson <weuxfya@gmail.com>
2019-11-22 14:39:12 -0700

    Add function to prompt for new items and update the db

--- server.js
+++ server.js
@@ -4,9 +4,7 @@ const inquirer = require('inquirer');
 // Create the connection information
 const connection = mysql.createConnection({
     host: 'localhost',
-
     port: 3306,
-
     user: 'root',
     password: 'hmtdih2tym!',
     database: 'greatbay_db'
@@ -19,5 +17,60 @@ connection.connect((err) => {
 });
 
 const start = () => {
-    console.log('in start')
+    inquirer.prompt({
+        name: 'postOrBid',
+        type: 'list',
+        message: 'Would you like to Post and auction or Bid on an auction?',
+        choices: ['POST', 'BID', 'EXIT']
+    }).then((answer) => {
+        if (answer.postOrBid === 'POST') {
+            postAuction();
+        } else if (answer.postOrBid === 'BID') {
+            bidAuction()
+        } else {
+            connection.end();
+        }
+    });
+
+}
+
+const postAuction = () => {
+    inquirer.prompt([{
+        name: 'item',
+        type: 'input',
+        message: 'What is the item you want to post for auction?'
+    },
+    {
+        name: 'category',
+        type: 'input',
+        message: 'What category would you like to put your auction in?'
+    },
+    {
+        name: 'startingBid',
+        type: 'input',
+        message: 'What would you like your starting bid to be?',
+        validate: function (value) {
+            if (isNaN(value) === false) {
+                return true;
+            }
+            return false;
+        }
+    }
+
+    ]).then((answer) => {
+        connection.query(
+            "INSERT INTO auctions SET ?",
+            {
+                item_name: answer.item,
+                category: answer.category,
+                starting_bid: answer.startingBid || 0,
+                highest_bid: answer.highest_bid || 0
+            },
+            (err) => {
+                if (err) throw err;
+                console.log('Your auction was successfully created..');
+                start();
+            }
+        );
+    });
 }
